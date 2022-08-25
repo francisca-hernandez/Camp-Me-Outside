789ccfd9f5a7b41cbf9ddcc558d64211e4fccbc9
joseph <j.hernandez.92189@gmail.com>
2022-08-24 19:00:08 -0700

    added for loop to generate cards for each search result

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,11 +21,40 @@ document.addEventListener('DOMContentLoaded', () => {
   
   });
 
+  let url = "https://ridb.recreation.gov/api/v1/facilities?limit=5&offset=0&state=WA&radius=10&activity=CAMPING&lastupdated=10-01-2018&apikey=";
+  let $campsiteContainer = document.getElementById("campsite-container");
+  let $modalContainer = document.querySelector(".modal-container");
 
-  fetch("https://ridb.recreation.gov/api/v1/campsites?query=Overnight&limit=50&offset=0&apikey=ad1485d4-8c3a-403d-8244-10d0d8498353").then(function(response) {
+
+  fetch(url).then(function(response) {
     if (response.ok) {
       response.json().then(function(data) {
-        console.log(data);
+        console.log(data.RECDATA);
+
+        for (i = 0; i < data.RECDATA.length; i++) {
+          let $campsiteCard = document.createElement("div")
+          $campsiteCard.classList = ("card my-1");
+
+          let $campsiteName = document.createElement("h3");
+          $campsiteName.classList = ("card-header-title");
+          $campsiteName.textContent = data.RECDATA[i].FacilityName;
+
+          let $campsiteDescription = document.createElement("p");
+          $campsiteDescription.classList = ("card-content");
+          $campsiteDescription.innerHTML = data.RECDATA[i].FacilityDescription;
+
+          $campsiteCard.appendChild($campsiteName);
+          $campsiteCard.appendChild($campsiteDescription);
+          $campsiteContainer.appendChild($campsiteCard);
+
+          
+          // $campsiteCard.id = ("modal" + [i]);
+          // $campsiteCard.classList = ("modal");
+          
+
+
+        };
       });
     };
   });
+
