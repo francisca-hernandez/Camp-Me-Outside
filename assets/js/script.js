b050f1990438bdf36719341b973ad108b836f334
David. Ricksgers <david@gmail.com>
2022-08-26 19:05:19 -0700

    updating Reddit with Develop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,66 @@
 // Nav Bar - Making it so the bar shows up on the left 
 document.addEventListener('DOMContentLoaded', () => {
 
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  
-    // Add a click event on each of them
-    $navbarBurgers.forEach( el => {
-      el.addEventListener('click', () => {
-  
-        // Get the target from the "data-target" attribute
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
-  
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-  
-      });
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Add a click event on each of them
+  $navbarBurgers.forEach( el => {
+    el.addEventListener('click', () => {
+
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+
     });
-  
   });
-  
+
+
+
+let url = "https://ridb.recreation.gov/api/v1/facilities?limit=5&offset=0&state=WA&radius=10&activity=CAMPING&lastupdated=10-01-2018&apikey=ad1485d4-8c3a-403d-8244-10d0d8498353";
+let $campsiteContainer = document.getElementById("campsite-container");
+let $modalContainer = document.querySelector(".modal-container");
+
+
+fetch(url).then(function(response) {
+  if (response.ok) {
+    response.json().then(function(data) {
+      console.log(data.RECDATA);
+
+      for (i = 0; i < data.RECDATA.length; i++) {
+        let $campsiteCard = document.createElement("div")
+        $campsiteCard.classList = ("card my-1");
+
+        let $campsiteName = document.createElement("h3");
+        $campsiteName.classList = ("card-header-title");
+        $campsiteName.textContent = data.RECDATA[i].FacilityName;
+     
+        let $campsiteDescription = document.createElement("p");
+        $campsiteDescription.classList = ("card-content");
+        $campsiteDescription.innerHTML = data.RECDATA[i].FacilityDescription;
+
+        $campsiteCard.appendChild($campsiteName);
+        $campsiteCard.appendChild($campsiteDescription);
+        $campsiteContainer.appendChild($campsiteCard);
+
+        
+        // $campsiteCard.id = ("modal" + [i]);
+        // $campsiteCard.classList = ("modal");
+
+        // $modalContainer.appendChild($campsiteCard);
+
+        
+        
+
+
+      };
+    });
+  };
+});
+
+});
+
--- scrip2.js
+++
@@ -1,7 +0,0 @@
-fetch("https://www.reddit.com/r/CampFireStories.json").then(function (response) {
-    if (response.ok) {
-        response.json().then(function (data) {
-            console.log(data)
-        });
-    };
-});
