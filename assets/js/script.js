e1bc1e1898be53ad3a0ff41b70292e66d26e13f8
joseph <j.hernandez.92189@gmail.com>
2022-08-27 11:22:41 -0700

    modal now pops up on button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,72 +1,72 @@
 // Nav Bar - Making it so the bar shows up on the left 
 document.addEventListener('DOMContentLoaded', () => {
 
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  
-    // Add a click event on each of them
-    $navbarBurgers.forEach( el => {
-      el.addEventListener('click', () => {
-  
-        // Get the target from the "data-target" attribute
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
-  
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-  
-      });
-    });
-  
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
-// API for Campsites 
+  // Add a click event on each of them
+  $navbarBurgers.forEach(el => {
+    el.addEventListener('click', () => {
+
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+
+    });
+  });
+
+
+  // API for Campsites 
 
   let url = "https://ridb.recreation.gov/api/v1/facilities?limit=5&offset=0&state=WA&radius=10&activity=CAMPING&lastupdated=10-01-2018&apikey=ad1485d4-8c3a-403d-8244-10d0d8498353";
   let $campsiteContainer = document.getElementById("campsite-container");
   let $usStatesContainer = document.querySelector("#us-states-container");
   let $modalContainer = document.querySelector(".modal-container");
- 
-// event listener for when options are changed
-  $usStatesContainer.addEventListener('change', function(event) {
+
+  // event listener for when options are changed
+  $usStatesContainer.addEventListener('change', function (event) {
 
     let $selectedState = $usStatesContainer.value;
 
     let url = "https://ridb.recreation.gov/api/v1/facilities?limit=5&offset=0&state=" + $selectedState + "&radius=10&activity=CAMPING&lastupdated=10-01-2018&apikey=ad1485d4-8c3a-403d-8244-10d0d8498353";
-    
-    
+
+
 
 
     console.log(event.target)
     console.log($usStatesContainer.value);
 
-    fetch(url).then(function(response) {
+    fetch(url).then(function (response) {
       if (response.ok) {
-        response.json().then(function(data) {
+        response.json().then(function (data) {
           console.log(data.RECDATA);
-  
+
           for (i = 0; i < data.RECDATA.length; i++) {
             let $campsiteCard = document.createElement("div");
             // $campsiteCard.classList = ("card my-1");
-  
+
             // let $campsiteName = document.createElement("h3");
             // $campsiteName.classList = ("card-header-title");
             // $campsiteName.textContent = data.RECDATA[i].FacilityName;
-         
+
             // let $campsiteDescription = document.createElement("p");
             // $campsiteDescription.classList = ("card-content");
             // $campsiteDescription.innerHTML = data.RECDATA[i].FacilityDescription;
-  
+
             // $campsiteCard.appendChild($campsiteName);
             // $campsiteCard.appendChild($campsiteDescription);
             // $campsiteContainer.appendChild($campsiteCard);
-  
+
             // create modal div within modal container
             $campsiteCard.id = ("modal" + [i]);
             $campsiteCard.classList = ("modal");
-            console.log($campsiteCard);
+            
             $modalContainer.appendChild($campsiteCard);
-  
+
             let $modalBackground = document.createElement("div");
             $modalBackground.classList = ("modal-background");
             $campsiteCard.appendChild($modalBackground);
@@ -82,7 +82,7 @@ document.addEventListener('DOMContentLoaded', () => {
             $modalCardTitle.textContent = data.RECDATA[i].FacilityName;
 
             $modalCardHeader.appendChild($modalCardTitle);
-            $modalContentContainer.appendChild($modalCardHeader);
+            $campsiteCard.appendChild($modalCardHeader);
 
             let $modalTargetButton = document.createElement("button");
             $modalTargetButton.classList = ("js-modal-trigger");
@@ -93,11 +93,52 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-  
-            
-            
-  
-  
+            // Functions to open and close a modal
+            function openModal($el) {
+              $el.classList.add('is-active');
+            }
+
+            function closeModal($el) {
+              $el.classList.remove('is-active');
+            }
+
+            function closeAllModals() {
+              (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+                closeModal($modal);
+              });
+            }
+
+            // Add a click event on buttons to open a specific modal
+            (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+              const modal = $trigger.dataset.target;
+              const $target = document.getElementById(modal);
+
+              $trigger.addEventListener('click', () => {
+                openModal($target);
+              });
+            });
+
+            // Add a click event on various child elements to close the parent modal
+            (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+              const $target = $close.closest('.modal');
+
+              $close.addEventListener('click', () => {
+                closeModal($target);
+              });
+            });
+
+            // Add a keyboard event to close all modals
+            document.addEventListener('keydown', (event) => {
+              const e = event || window.event;
+
+              if (e.keyCode === 27) { // Escape key
+                closeAllModals();
+              }
+            });
+
+
+
+
           };
         });
       };
@@ -105,6 +146,6 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 
 
-  });
+});
 
 
