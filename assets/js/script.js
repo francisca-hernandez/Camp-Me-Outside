ba77aa30205bba0c9dca6ca5fed7e36b9fda04c7
David. Ricksgers <davidricksgers@gmail.com>
2022-08-28 09:30:07 -0700

    Redit cardsconverted to modals.

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,25 +28,25 @@ document.addEventListener('DOMContentLoaded', () => {
   let $usStatesContainer = document.querySelector("#us-states-container");
   let $modalContainer = document.querySelector(".modal-container");
 
-    // event listener for when options are changed
-    $usStatesContainer.addEventListener('change', function (event) {
+  // event listener for when options are changed
+  $usStatesContainer.addEventListener('change', function (event) {
 
 
-      let $selectedState = $usStatesContainer.value;
+    let $selectedState = $usStatesContainer.value;
 
-      let url = "https://ridb.recreation.gov/api/v1/facilities?limit=5&offset=0&state=" + $selectedState + "&radius=10&activity=CAMPING&lastupdated=10-01-2018&apikey=ad1485d4-8c3a-403d-8244-10d0d8498353";
+    let url = "https://ridb.recreation.gov/api/v1/facilities?limit=5&offset=0&state=" + $selectedState + "&radius=10&activity=CAMPING&lastupdated=10-01-2018&apikey=ad1485d4-8c3a-403d-8244-10d0d8498353";
 
     fetch(url).then(function (response) {
       if (response.ok) {
         response.json().then(function (data) {
           console.log(data.RECDATA);
-          
-           while ($campsiteContainer.hasChildNodes()) {
-              $campsiteContainer.removeChild($campsiteContainer.firstChild);
-            }
-            while ($modalContainer.hasChildNodes()) {
-              $modalContainer.removeChild($modalContainer.firstChild);
-            }
+
+          while ($campsiteContainer.hasChildNodes()) {
+            $campsiteContainer.removeChild($campsiteContainer.firstChild);
+          }
+          while ($modalContainer.hasChildNodes()) {
+            $modalContainer.removeChild($modalContainer.firstChild);
+          }
 
           for (i = 0; i < data.RECDATA.length; i++) {
             let $campsiteCard = document.createElement("div");
@@ -54,7 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
             // create modal div within modal container
             $campsiteCard.id = ("modal" + [i]);
             $campsiteCard.classList = ("modal");
-            
+
             $modalContainer.appendChild($campsiteCard);
 
             // create div to set background within modal
@@ -82,7 +82,7 @@ document.addEventListener('DOMContentLoaded', () => {
             // create description
             let $modalDescriptionContainer = document.createElement("section");
             $modalDescriptionContainer.classList = ("modal-card-body");
-            
+
             let $modalDescriptionText = document.createElement("p");
             $modalDescriptionText.innerHTML = data.RECDATA[i].FacilityDescription;
             $modalDescriptionContainer.appendChild($modalDescriptionText);
@@ -154,36 +154,116 @@ document.addEventListener('DOMContentLoaded', () => {
       };
     });
   });
-           
 
+  //API for reddit
 
+  let rditurl = "https://www.reddit.com/r/CampFireStories.json?limit=5"
+  let $rditContainer = document.getElementById("campstory-container");
+  let $rditModalContainer = document.querySelector(".modal-container");
 
-    let rditurl = "https://www.reddit.com/r/CampFireStories.json?limit=5"
-    let $rditContainer = document.getElementById("campstory-container");
+  fetch(rditurl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data.data.children);
 
-    fetch(rditurl).then(function (response) {
-      if (response.ok) {
-        response.json().then(function (data) {
-          console.log(data.data.children);
+        for (i = 0; i < data.data.children.length; i++) {
+          let $rditCard = document.createElement("div")
 
-          for (i = 0; i < data.data.children.length; i++) {
-            let $rditCard = document.createElement("div")
-            $rditCard.classList = ("card my-1");
+          // create modal div within modal container
+          $rditCard.id = ("modal" + [i]);
+          $rditCard.classList = ("modal");
 
-            let $rditName = document.createElement("h3");
-            $rditName.classList = ("card-header-title");
-            $rditName.textContent = data.data.children[i].data.title;
+          $rditModalContainer.appendChild($rditCard);
 
-            let $rditDescription = document.createElement("p");
-            $rditDescription.classList = ("card-content");
-            $rditDescription.innerHTML = data.data.children[i].data.selftext;
+          // create div to set background within modal
+          let $modalBackground = document.createElement("div");
+          $modalBackground.classList = ("modal-background");
+          $rditCard.appendChild($modalBackground);
 
-            $rditCard.appendChild($rditName);
-            $rditCard.appendChild($rditDescription);
-            $rditContainer.appendChild($rditCard);
-          };
-        });
-      };
-    });
+          // create div to hold all card items
+          let $modalCard = document.createElement("div");
+          $modalCard.classList = ("modal-card");
+          $rditCard.appendChild($modalCard);
 
+          // create div to contain header and description
+          let $rditModalContentContainer = document.createElement("div");
+          $rditModalContentContainer.classList = ("modal-card");
+
+          // create header
+          let $modalCardHeader = document.createElement("header");
+          $modalCardHeader.classList = ("modal-card-head")
+
+          let $modalCardTitle = document.createElement("p");
+          $modalCardTitle.classList = ("modal-card-title");
+          $modalCardTitle.textContent = data.data.children[i].data.title;
+
+          // create description
+          let $modalDescriptionContainer = document.createElement("section");
+          $modalDescriptionContainer.classList = ("modal-card-body");
+
+          let $modalDescriptionText = document.createElement("p");
+          $modalDescriptionText.innerHTML = data.data.children[i].data.selftext;
+
+          $modalDescriptionContainer.appendChild($modalDescriptionText);
+
+          // append information into card
+          $modalCardHeader.appendChild($modalCardTitle);
+          $modalCard.appendChild($modalCardHeader);
+          $modalCard.appendChild($modalDescriptionContainer);
+
+          // create button to display on page when clicked
+          let $modalTargetButton = document.createElement("button");
+          $modalTargetButton.classList = ("js-modal-trigger button is-primary");
+          $modalTargetButton.setAttribute("data-target", $rditCard.id)
+          $modalTargetButton.textContent = data.data.children[i].data.title;
+
+          // append button to page
+          $rditContainer.appendChild($modalTargetButton);
+
+          // Functions to open and close a modal
+          function openModal($el) {
+            $el.classList.add('is-active');
+          }
+
+          function closeModal($el) {
+            $el.classList.remove('is-active');
+          }
+
+          function closeAllModals() {
+            (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+              closeModal($modal);
+            });
+          }
+
+          // Add a click event on buttons to open a specific modal
+          (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+            const modal = $trigger.dataset.target;
+            const $target = document.getElementById(modal);
+
+            $trigger.addEventListener('click', () => {
+              openModal($target);
+            });
+          });
+
+          // Add a click event on various child elements to close the parent modal
+          (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+            const $target = $close.closest('.modal');
+
+            $close.addEventListener('click', () => {
+              closeModal($target);
+            });
+          });
+
+          // Add a keyboard event to close all modals
+          document.addEventListener('keydown', (event) => {
+            const e = event || window.event;
+
+            if (e.keyCode === 27) { // Escape key
+              closeAllModals();
+            }
+          });
+        };
+      });
+    };
   });
+});
