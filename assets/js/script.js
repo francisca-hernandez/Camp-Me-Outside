8334a6f84d4b01d9d9d77a1f6f98f04cad681b0a
David. Ricksgers <davidricksgers@gmail.com>
2022-08-30 01:01:01 -0700

    got favorites feature working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+const favCampsites = [];
 // Nav Bar - Making it so the bar shows up on the left 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -26,7 +27,15 @@ document.addEventListener('DOMContentLoaded', () => {
   let $campsiteContainer = document.getElementById("campsite-container");
   let $usStatesContainer = document.querySelector("#us-states-container");
   let $modalContainer = document.querySelector(".modal-container");
- 
+  let $favoriteCampsites = document.querySelector("#favorite-Campsites");
+  const storedFavs = JSON.parse(localStorage.getItem("facility-id"));
+  const favCampsites = [];
+  for (var i = 0; i < storedFavs.length; i++) {
+    console.log(storedFavs[i]);
+    favCampsites.push(storedFavs[i])
+    localStorage.setItem("facility-id", JSON.stringify(favCampsites));
+  }
+
   // event listener for when options are changed
   $usStatesContainer.addEventListener('change', function (event) {
 
@@ -35,18 +44,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let url = "https://ridb.recreation.gov/api/v1/facilities?limit=5&offset=0&state=" + $selectedState + "&radius=10&activity=CAMPING&lastupdated=10-01-2018&apikey=ad1485d4-8c3a-403d-8244-10d0d8498353";
 
-
     fetch(url).then(function (response) {
       if (response.ok) {
         response.json().then(function (data) {
           console.log(data.RECDATA);
-
-          // while ($campsiteContainer.hasChildNodes()) {
-          //   $campsiteContainer.removeChild($campsiteContainer.firstChild);
-          // }
-          // while ($modalContainer.hasChildNodes()) {
-          //   $modalContainer.removeChild($modalContainer.firstChild);
-          // }
 
           for (i = 0; i < data.RECDATA.length; i++) {
             let $campsiteCard = document.createElement("div");
@@ -94,7 +95,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
             $modalSaveButton.addEventListener("click", function () {
               console.log($facilityId);
-              localStorage.setItem("facility-id", $facilityId);
+              favCampsites.push($facilityId)
+              localStorage.setItem("facility-id", JSON.stringify(favCampsites));
+              console.log(favCampsites)
             });
 
 
@@ -181,8 +184,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //API for reddit
     let rditurl = "https://www.reddit.com/r/CampFireStories.json?limit=5"
-  let $rditContainer = document.getElementById("campstory-container");
-  let $rditModalContainer = document.querySelector(".modal-container");
+    let $rditContainer = document.getElementById("campstory-container");
+    let $rditModalContainer = document.querySelector(".modal-container");
 
     fetch(rditurl).then(function (response) {
       if (response.ok) {
@@ -303,26 +306,169 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
+
+  // function to add to local storage
+
+
   // function to pull from local storage
 
-  var pullFavoriteCampsite = function () {
-    let $storageFacilityId = localStorage.getItem("facility-id")
-    console.log($storageFacilityId);
+  $favoriteCampsites.addEventListener("click", function (event) {
 
-    let $storageUrl = "https://ridb.recreation.gov/api/v1/facilities/" + $storageFacilityId + "?full=true&apikey=ad1485d4-8c3a-403d-8244-10d0d8498353";
-    fetch($storageUrl).then(function (response) {
-      if (response.ok) {
-        response.json().then(function (data) {
-          console.log(data);
+    while ($campsiteContainer.hasChildNodes()) {
+      $campsiteContainer.removeChild($campsiteContainer.firstChild);
+    }
+    while ($modalContainer.hasChildNodes()) {
+      $modalContainer.removeChild($modalContainer.firstChild);
+    }
+
+    const storedFavs = JSON.parse(localStorage.getItem("facility-id"));
+    console.log(storedFavs)
+    for (var i = 0; i < storedFavs.length; i++) {
+      console.log(storedFavs[i]);
+
+      let $storageUrl = "https://ridb.recreation.gov/api/v1/facilities/" + storedFavs[i] + "?full=true&apikey=ad1485d4-8c3a-403d-8244-10d0d8498353";
+      
+      fetch($storageUrl).then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+            console.log(data);
+
+
+            // begin dynamically creating elements with data
+
+            let $campsiteCard = document.createElement("div");
+
+            // create modal div within modal container
+            $campsiteCard.id = ("modal" + [i]);
+            $campsiteCard.classList = ("modal");
+
+            $modalContainer.appendChild($campsiteCard);
+
+            // create div to set background within modal
+            let $modalBackground = document.createElement("div");
+            $modalBackground.classList = ("modal-background");
+            $campsiteCard.appendChild($modalBackground);
+
+            // create div to hold all card items
+            let $modalCard = document.createElement("div");
+            $modalCard.classList = ("modal-card");
+            $campsiteCard.appendChild($modalCard);
+
+            // create div to contain header and description
+            let $modalContentContainer = document.createElement("div");
+            $modalContentContainer.classList = ("modal-card");
+
+            // create header
+            let $modalCardHeader = document.createElement("header");
+            $modalCardHeader.classList = ("modal-card-head")
+
+            let $modalCardTitle = document.createElement("p");
+            $modalCardTitle.classList = ("modal-card-title");
+            $modalCardTitle.textContent = data.FacilityName;
+
+
+
+            // need to change to remove favorite.
           
 
-          // begin dynamically creating elements with data
 
-        })
-      }
-    })
+            let $modalSaveButton = document.createElement("button");
+            $modalSaveButton.classList = ("button is-link");
+            $modalSaveButton.setAttribute("id", "campsite-save-button" + [i]);
+            $modalSaveButton.textContent = "Favorite";
 
-  };
-  // event listener is going to be what calls this function to append the page.
-  pullFavoriteCampsite();
+            let $facilityId = [i].FacilityID;
+
+            $modalSaveButton.addEventListener("click", function () {
+              console.log($facilityId);
+              favCampsites.push($facilityId)
+              localStorage.setItem("facility-id", JSON.stringify(favCampsites));
+              console.log(favCampsites)
+            });
+
+
+
+
+
+
+            // create description
+            let $modalDescriptionContainer = document.createElement("section");
+            $modalDescriptionContainer.classList = ("modal-card-body");
+
+            let $modalDescriptionText = document.createElement("p");
+            $modalDescriptionText.innerHTML = data.FacilityDescription;
+            $modalDescriptionContainer.appendChild($modalDescriptionText);
+
+            // append information into card
+            $modalCardHeader.appendChild($modalCardTitle);
+            $modalCardHeader.appendChild($modalSaveButton);
+            $modalCard.appendChild($modalCardHeader);
+            $modalCard.appendChild($modalDescriptionContainer);
+
+            // create button to display on page when clicked
+            let $modalTargetButton = document.createElement("button");
+            $modalTargetButton.classList = ("js-modal-trigger button is-fullwidth is-responsive");
+            $modalTargetButton.setAttribute("data-target", $campsiteCard.id)
+            $modalTargetButton.textContent = data.FacilityName;
+
+            // append button to page
+            $campsiteContainer.appendChild($modalTargetButton);
+
+
+
+            // Functions to open and close a modal
+            function openModal($el) {
+              $el.classList.add('is-active');
+            }
+
+            function closeModal($el) {
+              $el.classList.remove('is-active');
+            }
+
+            function closeAllModals() {
+              (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+                closeModal($modal);
+              });
+            }
+
+            // Add a click event on buttons to open a specific modal
+            (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+              const modal = $trigger.dataset.target;
+              const $target = document.getElementById(modal);
+
+              $trigger.addEventListener('click', () => {
+                openModal($target);
+              });
+            });
+
+            // Add a click event on various child elements to close the parent modal
+            (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+              const $target = $close.closest('.modal');
+
+              $close.addEventListener('click', () => {
+                closeModal($target);
+              });
+            });
+
+            // Add a keyboard event to close all modals
+            document.addEventListener('keydown', (event) => {
+              const e = event || window.event;
+
+              if (e.keyCode === 27) { // Escape key
+                closeAllModals();
+              }
+            });
+
+
+
+          });
+        };
+      });
+    };
+  });
 });
+
+
+
+
+
